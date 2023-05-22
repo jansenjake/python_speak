f0bc7b09e8cf779a6dc60b61a8ccb9afdc8f8191
jansenjake <jansenjake599@gmail.com>
2023-05-22 11:25:05 +0200

    first py push

---
+++ app.py
@@ -0,0 +1,34 @@
+# age = 20
+# price = 19.95
+# first_name = 'Jake'
+# is_online = True
+
+# print(age)
+
+# name = 'Jake'
+# surname = 'Jansen'
+# age = 20
+# patient_status = 'new'
+
+
+# name = input('What is your name? ')
+# print('Hello ' + name)
+
+
+################################ pip_attempt #################################
+
+# import pyttsx3
+
+# def say(text) : 
+#     engine.say(text)
+#     print(text)
+#     engine.runAndWait()
+
+#     engine = pyttsx3.init()
+#     say('Python is the best programming language')
+#     say('Jake is a dog!')
+#     say('Anyway, what is for lunch?')
+
+################################ pip_attempt #################################
+
+
